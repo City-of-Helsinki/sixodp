6a1672c5db442f45aed72b691b22c38bf61ee3a2
Mikko Nieminen <mikko.nieminen@gofore.com>
2018-02-16 17:14:33 +0200

    Remove eu-logos, fix theme colors, change vagrant ip to unique

--- sixodp/header.php
+++ sixodp/header.php
@@ -64,13 +64,4 @@
     <a href="<?php echo site_url(); ?>" class="logo--brand">
       <img src="<?php echo assets_url(); ?>/images/hero_logo.png" />
     </a>
-    <div class="logos--eu">
-      <div class="logo--erdf">
-        <img src="<?php echo assets_url(); ?>/images/EU_ERDF_FI.png" alt="European Regional Development Fund logo">
-      </div>
-      <div class="logo--eu">
-        <img src="<?php echo assets_url(); ?>/images/LeverageEU_FI.png" alt="Leverage with EU logo">
-      </div>
-      <div class="flag-ends"></div>
-    </div>
   </div>
