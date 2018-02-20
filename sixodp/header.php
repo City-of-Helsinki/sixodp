5e0a061bfe47257c1f979a26e0ed36f22ac85002
Jari Voutilainen <jari.voutilainen@iki.fi>
2018-02-20 16:15:54 +0200

    Add tag manager and hotjar to templates

--- sixodp/header.php
+++ sixodp/header.php
@@ -28,9 +28,24 @@
 		var locale_ckan = '<?php echo get_current_locale_ckan() ?>';
 	</script>
 	<link rel="shortcut icon" href="/assets/images/favicon.ico" />
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-W9PV285');</script>
+    <!-- End Google Tag Manager -->
+
+    <!-- Hotjar Tracking Code for hri.fi -->
+    <script> (function(h,o,t,j,a,r){ h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)}; h._hjSettings={hjid:729166,hjsv:6}; a=o.getElementsByTagName('head')[0]; r=o.createElement('script');r.async=1; r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv; a.appendChild(r); })(window,document,'static.hotjar.com/c/hotjar-','.js?sv='); </script>
 </head>
 
 <body <?php body_class(); ?> id="wordpress-indicator">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W9PV285"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
   <div id="mobile-indicator"></div>
   <?php $notifications = get_posts(array('post_type' => 'notification')); ?>
   <?php $extra_classes = count($notifications) > 0 ? ' has-notification' : '' ?>
