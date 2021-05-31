997efd0592ce29df030e4dd404060348984242d1
Jari Voutilainen <jari.voutilainen@iki.fi>
2021-05-31 12:27:05 +0300

    Remove google tag manager

--- sixodp/header.php
+++ sixodp/header.php
@@ -28,22 +28,9 @@
 		var locale_ckan = '<?php echo get_current_locale_ckan() ?>';
 	</script>
 	<link rel="shortcut icon" href="/assets/images/favicon.ico" />
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-W9PV285');</script>
-    <!-- End Google Tag Manager -->
-
 </head>
 
 <body <?php body_class(); ?> id="wordpress-indicator">
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W9PV285"
-                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
-
   <div id="mobile-indicator"></div>
   <?php $notifications = get_posts(array('post_type' => 'notification')); ?>
   <?php $extra_classes = count($notifications) > 0 ? ' has-notification' : '' ?>
