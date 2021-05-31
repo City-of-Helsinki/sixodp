c0dce3b3ae313fcd28dec78d1917771c02e9603c
Jari Voutilainen <jari.voutilainen@iki.fi>
2021-05-31 08:42:45 +0300

    Remove hotjar from wp

--- sixodp/header.php
+++ sixodp/header.php
@@ -36,8 +36,6 @@
         })(window,document,'script','dataLayer','GTM-W9PV285');</script>
     <!-- End Google Tag Manager -->
 
-    <!-- Hotjar Tracking Code for hri.fi -->
-    <script> (function(h,o,t,j,a,r){ h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)}; h._hjSettings={hjid:729166,hjsv:6}; a=o.getElementsByTagName('head')[0]; r=o.createElement('script');r.async=1; r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv; a.appendChild(r); })(window,document,'static.hotjar.com/c/hotjar-','.js?sv='); </script>
 </head>
 
 <body <?php body_class(); ?> id="wordpress-indicator">
